ac1c219c7caccb38cd28ed58e56136747cf86927
Deceitful1 <gfelipe0555@gmail.com>
2024-05-07 18:50:46 -0300

    fiz um metodo de soma

--- src/Main.java
+++ src/Main.java
@@ -5,5 +5,15 @@ public class Main {
         System.out.println("Hello world!");
 
         Scanner sc = new Scanner(System.in);
+        int numero1 = sc.nextInt();
+        int numero2 = sc.nextInt();
+        System.out.println(Soma(numero1,numero2));
+
+
     }
+    public static int Soma(int a, int b)
+    {
+        return a + b;
+    }
+
 }
